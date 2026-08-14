aa31f610c5580241f315ac1c60fa2ed511daa9ce
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-14 14:21:00 -0500

    Forzar carga de la versión completa del editor de mesas

--- app_integral/js/modules/invitados/ui-copy.js
+++ app_integral/js/modules/invitados/ui-copy.js
@@ -167,7 +167,7 @@
   if (document.querySelector('script[data-mgd-tables-editor-loader]')) return;
   const script = document.createElement('script');
   script.type = 'module';
-  script.src = new URL('js/modules/invitados/tables-editor-entry.js?v=20260814-1418-order1', document.baseURI).href;
+  script.src = new URL('js/modules/invitados/tables-editor-entry.js?v=20260814-1438-tables2', document.baseURI).href;
   script.dataset.mgdTablesEditorLoader = '1';
   document.head.appendChild(script);
 })();
