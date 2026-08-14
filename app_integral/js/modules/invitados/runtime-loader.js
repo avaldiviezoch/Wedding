88e324df2891f9ec563a5929dafdcc505f1cedf1
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-14 16:43:26 -0500

    Bust tables legacy UI loader cache

--- app_integral/js/modules/invitados/runtime-loader.js
+++ app_integral/js/modules/invitados/runtime-loader.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '20260814-1633-invitadoslegacy2';
+  const VERSION = '20260814-1645-exactlegacy1';
   let loading = null;
 
   function moduleUrl(file, version) {
@@ -13,7 +13,7 @@
     loading = Promise.all([
       import(moduleUrl('index.js', '20260814-1242-rsvp2')),
       import(moduleUrl('ui-copy.js', '20260814-1532-uicopy3')),
-      import(moduleUrl('tables-lazy-loader.js', '20260814-1633-legacyskin2'))
+      import(moduleUrl('tables-lazy-loader.js', '20260814-1645-exactlegacy1'))
     ]).catch((error) => {
       console.error('No se pudo iniciar Invitados:', error);
       loading = null;
