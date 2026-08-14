dcf9c242da3d9c0c2c939d0501f3e0ecc708c2c5
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-14 16:57:01 -0500

    Renovar cache final de mesas

--- wedding-sw.js
+++ wedding-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wedding-static-v3-20260814-neweditor-oldlook';
+const CACHE_NAME = 'wedding-static-v4-20260814-neweditor-oldlook2';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
