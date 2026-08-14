1299943aa3b2d6d9dc7d6d3531e1b96a38dcea91
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-14 16:56:54 -0500

    Versionar runtime final del editor de mesas

--- app_integral/js/modules/invitados/runtime-loader.js
+++ app_integral/js/modules/invitados/runtime-loader.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '20260814-1708-neweditor-oldlook1';
+  const VERSION = '20260814-1712-neweditor-oldlook2';
   let loading = null;
 
   function moduleUrl(file, version) {
@@ -13,7 +13,7 @@
     loading = Promise.all([
       import(moduleUrl('index.js', '20260814-1242-rsvp2')),
       import(moduleUrl('ui-copy.js', '20260814-1532-uicopy3')),
-      import(moduleUrl('tables-lazy-loader.js', '20260814-1708-neweditor-oldlook1'))
+      import(moduleUrl('tables-lazy-loader.js', '20260814-1712-neweditor-oldlook2'))
     ]).catch((error) => {
       console.error('No se pudo iniciar Invitados:', error);
       loading = null;
