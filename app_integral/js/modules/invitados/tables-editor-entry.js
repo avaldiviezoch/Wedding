484a4ea78eef15e9e668a38f8cc6b9ee13c9911d
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-14 14:20:03 -0500

    Activar autoguardado de edición de mesas

--- app_integral/js/modules/invitados/tables-editor-entry.js
+++ app_integral/js/modules/invitados/tables-editor-entry.js
@@ -1,3 +1,4 @@
 import './tables-data-guard.js?v=20260814-1359-guard1';
 await import('./tables-editor.js?v=20260814-1354-tables1');
 await import('./tables-order.js?v=20260814-1422-order2');
+await import('./tables-live-edit.js?v=20260814-1431-live1');
