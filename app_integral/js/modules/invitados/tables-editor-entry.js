63757af81f87827cdb206d0be50e97227a770979
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-14 14:18:08 -0500

    Actualizar versión de ordenamiento de mesas

--- app_integral/js/modules/invitados/tables-editor-entry.js
+++ app_integral/js/modules/invitados/tables-editor-entry.js
@@ -1,3 +1,3 @@
 import './tables-data-guard.js?v=20260814-1359-guard1';
 await import('./tables-editor.js?v=20260814-1354-tables1');
-await import('./tables-order.js?v=20260814-1418-order1');
+await import('./tables-order.js?v=20260814-1422-order2');
