b5da8266dbda7270727b760b22a63d229a2b0e9b
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-16 14:24:10 -0500

    Forzar carga del loader final de Mesas

--- app_integral/js/modules/invitados/runtime-loader.js
+++ app_integral/js/modules/invitados/runtime-loader.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '20260814-1745-no-legacy-flash';
+  const VERSION = '20260816-1510-final-style-ready';
   let loading = null;
 
   function moduleUrl(file, version) {
@@ -13,7 +13,7 @@
     loading = Promise.all([
       import(moduleUrl('index.js', '20260814-1242-rsvp2')),
       import(moduleUrl('ui-copy.js', '20260814-1532-uicopy3')),
-      import(moduleUrl('tables-lazy-loader.js', '20260814-1745-no-legacy-flash'))
+      import(moduleUrl('tables-lazy-loader.js', '20260816-1510-final-style-ready'))
     ]).catch((error) => {
       console.error('No se pudo iniciar Invitados:', error);
       loading = null;
