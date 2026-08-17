875010ff610374f43e1295a3ae741cf2d30c28b9
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-17 13:47:08 -0500

    Forzar carga del navegador determinista de módulos

--- app_integral/js/core/master-theme-runtime.js
+++ app_integral/js/core/master-theme-runtime.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '20260817-master-theme-runtime-v7-autosave';
+  const VERSION = '20260817-master-theme-runtime-v8-navfix';
   const WORKSPACE_ID = 'unifiedWorkspace';
   const STYLE_ID = 'mgdMasterThemeStyles';
   const CHECKLIST_STYLE_ID = 'mgdChecklistRefineStyles';
