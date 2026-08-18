dc07b2d2195ff7a0402b1a49b7de6da7d509d571
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-18 12:16:39 -0500

    Inv5: refrescar layout de notas ampliadas

--- invitaciones/invitacion_5/effects_5.js
+++ invitaciones/invitacion_5/effects_5.js
@@ -10,7 +10,7 @@
   if(!document.getElementById('inv5-layout-loader')){
     const layoutScript=document.createElement('script');
     layoutScript.id='inv5-layout-loader';
-    layoutScript.src='./layout_5.js?v=20260818-1153';
+    layoutScript.src='./layout_5.js?v=20260818-1215';
     document.head.appendChild(layoutScript);
   }
 
