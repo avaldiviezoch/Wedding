68ccabc62887f7cc604f7fd11abacaca84f29490
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-19 10:49:34 -0500

    chore(rsvp): refresh guided RSVP assets

--- app_integral/js/modules/invitados/rsvp-native-admin-patch.js
+++ app_integral/js/modules/invitados/rsvp-native-admin-patch.js
@@ -1,5 +1,5 @@
 const VERSION='20260816-1918-native-fast1';
-const WIDGET='https://avaldiviezoch.github.io/Wedding/app_integral/js/modules/invitados/rsvp-native-widget.js?v=20260819-2000-attendee-music1';
+const WIDGET='https://avaldiviezoch.github.io/Wedding/app_integral/js/modules/invitados/rsvp-native-widget.js?v=20260819-2100-rsvp-guide1';
 const done=new WeakSet();
 
 function tokenFrom(doc){
