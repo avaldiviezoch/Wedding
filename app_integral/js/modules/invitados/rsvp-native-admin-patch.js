d973b7e139da03554f38684cb28d839bd3fca3b1
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-08-19 10:07:15 -0500

    Actualiza versión del RSVP progresivo: app_integral/js/modules/invitados/rsvp-native-admin-patch.js

--- app_integral/js/modules/invitados/rsvp-native-admin-patch.js
+++ app_integral/js/modules/invitados/rsvp-native-admin-patch.js
@@ -1,5 +1,5 @@
 const VERSION='20260816-1918-native-fast1';
-const WIDGET='https://avaldiviezoch.github.io/Wedding/app_integral/js/modules/invitados/rsvp-native-widget.js?v=20260816-1845-native1';
+const WIDGET='https://avaldiviezoch.github.io/Wedding/app_integral/js/modules/invitados/rsvp-native-widget.js?v=20260819-1700-progressive-rsvp1';
 const done=new WeakSet();
 
 function tokenFrom(doc){
